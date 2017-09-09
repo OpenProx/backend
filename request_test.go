1dcb140ee8ec9a92cf079e613efa426e309deafe
n0berius <31741429+n0berius@users.noreply.github.com>
2017-09-09 16:23:57 +0200

    Added the CheckID field to the request token.

--- request_test.go
+++ request_test.go
@@ -3,19 +3,19 @@ package backend
 import "testing"
 
 func TestRequestToken(t *testing.T) {
-	token, err := GenerateRequestToken(2, 10)
+	token, err := GenerateRequestToken(2, 10, 2)
 	if err != nil {
 		t.Error(err)
 		return
 	}
 
-	p, id, _, err := DecodeRequestToken(token)
+	p, id, chkid, _, err := DecodeRequestToken(token)
 	if err != nil {
 		t.Error(err)
 		return
 	}
 
-	if p != 2 || id != 10 {
+	if p != 2 || id != 10 || chkid != 2 {
 		t.Errorf("Wrong values after decode 2/10 -> %d/%d", p, id)
 	}
 }
