e291d46966ce5060485db734435d52a103fce5c8
n0berius <31741429+n0berius@users.noreply.github.com>
2017-09-09 16:22:39 +0200

    Added function that returns checkable proxy.

--- database.go
+++ database.go
@@ -1,8 +1,13 @@
 package backend
 
 import (
+	"fmt"
+	"math/rand"
+	"time"
+
 	"github.com/asdine/storm"
 	"github.com/asdine/storm/codec/msgpack"
+	"github.com/asdine/storm/q"
 )
 
 // InitDatabase inits the database
@@ -21,6 +26,31 @@ func (i *Instance) InitDatabase() error {
 func (i *Instance) HasProxy(identifier string) bool {
 	var dbProx Proxy
 	return i.Database.One("Identifier", identifier, &dbProx) != storm.ErrNotFound
+}
+
+// GetCheckableProxy returns a new CheckRequest for a proxy
+func (i *Instance) GetCheckableProxy(uid int) (*CheckRequest, error) {
+	ago := time.Now().Unix() - 60*5
+
+	var found []Proxy
+	i.Database.Select(q.Lt("LastCheck", ago), q.Lt("ChecksLength", 5)).OrderBy("ChecksLength").Find(&found)
+
+	if len(found) == 0 {
+		return nil, fmt.Errorf("No checkable proxy found")
+	}
+
+	selected := found[rand.Intn(len(found))]
+	token, err := GenerateRequestToken(selected.ID, uid, selected.CheckID)
+	if err != nil {
+		return nil, err
+	}
+
+	return &CheckRequest{
+		Token:    token,
+		IP:       selected.IP,
+		Port:     selected.Port,
+		Protocol: selected.Protocol,
+	}, nil
 }
 
 func (i *Instance) AddChecks() {
